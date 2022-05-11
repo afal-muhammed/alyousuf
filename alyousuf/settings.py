4c7e60f8657b4d7f307aa98f7dde112b12351253
afal <afal@sayonetech.com>
2022-05-11 22:27:47 +0530

    staticfiles

--- alyousuf/settings.py
+++ alyousuf/settings.py
@@ -82,19 +82,13 @@ AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-MEDIA_URL = '/media/'
-
 
 WSGI_APPLICATION = 'alyousuf.wsgi.application'
 
@@ -144,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
